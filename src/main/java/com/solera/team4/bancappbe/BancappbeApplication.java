f2df8325d8a14b039acb48c8a9a835d3473c27a2
Adrià Rubio Coll <adria.rubio@solera.com>
2022-11-24 10:03:07 +0100

    First commit

---
+++ src/main/java/com/solera/team4/bancappbe/BancappbeApplication.java
@@ -0,0 +1,13 @@
+package com.solera.team4.bancappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancappbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancappbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solera/team4/bancappbe/BancappbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solera.team4.bancappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancappbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
